6eb6bab2c7af7b838f959f36cf6c7079de26a21c
Alessandra-N <nardella.alessandra1987@gmail.com>
2021-06-15 15:27:35 +0200

    three cards section complete

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,7 +10,20 @@
 var app = new Vue({
   el: "#app",
   data: {
-    jumboLinks: ["Home", "About", "Projects", "Process", "Testimonials"]
+    jumboLinks: ["Home", "About", "Projects", "Process", "Testimonials"],
+    threeCards: [{
+      img: "fas fa-user-friends",
+      title: "Human Capital",
+      parag: "Humanizing business: Harness the power of technology to improve the way people work."
+    }, {
+      img: "fas fa-chart-pie",
+      title: "Core Business",
+      parag: "It takes innovative approaches to transform, and run existing platforms."
+    }, {
+      img: "fas fa-tachometer-alt",
+      title: "Performance",
+      parag: "Achieving maximum impact and value from investments in finance and supply chain"
+    }]
   }
 });
 
--- src/app.js
+++ src/app.js
@@ -9,6 +9,24 @@ const app = new Vue ({
             "Process", 
             "Testimonials",
         ],
+
+        threeCards: [
+            {
+                img: "fas fa-user-friends",
+                title: "Human Capital",
+                parag: "Humanizing business: Harness the power of technology to improve the way people work.",
+            },
+            {
+                img: "fas fa-chart-pie",
+                title: "Core Business",
+                parag: "It takes innovative approaches to transform, and run existing platforms.",
+            },
+            {
+                img: "fas fa-tachometer-alt",
+                title: "Performance",
+                parag: "Achieving maximum impact and value from investments in finance and supply chain",
+            }
+        ]
     },
 
 })
